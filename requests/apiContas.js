c89ec6da0a4abbefaa46e88c37d823a629bf1ec0
Papai-teste <testeprojetogit@gmail.com>
2025-01-06 11:46:06 -0300

    Update apiContas.js

--- requests/apiContas.js
+++ requests/apiContas.js
@@ -2,6 +2,7 @@ const config = require('../config');
 const integracao = require('../db/listaIntegracao');
 const { createAxiosInstance } = require('./common/axiosFactory');
 const buildOptions = require('./common/optionsBuilder');
+const msg = require('../db/listaErros');
 
 const contas = createAxiosInstance(config.INTEGRATION_API_CONTAS_URL, integracao.API_CONTAS);
 
@@ -15,14 +16,20 @@ module.exports = {
             }
         };
 
-        const tokenContas = await contas.post(
-            config.INTEGRATION_API_CONTAS_TOKEN_URL,
-            new URLSearchParams({
-                'grant_type': 'client_credentials',
-                'scope': config.INTEGRATION_API_CONTAS_TOKEN_SCOPE
-            }).toString(),
-            tokenOptions
-        );
+        let tokenContas;
+        try {
+            tokenContas = await contas.post(
+                config.INTEGRATION_API_CONTAS_TOKEN_URL,
+                new URLSearchParams({
+                    'grant_type': 'client_credentials',
+                    'scope': config.INTEGRATION_API_CONTAS_TOKEN_SCOPE
+                }).toString(),
+                tokenOptions
+            );
+        } catch (error) {
+            console.error("Erro na obtenção do token no API Contas: " + error);
+            throw new Error(msg.GC65);
+        }
 
         const options = buildOptions({
             req,
@@ -32,6 +39,14 @@ module.exports = {
             }
         });
 
-        return await contas.get("/" + cpf, options);
+        let resultado;
+        try {
+            resultado = await contas.get("/" + cpf, options);
+        } catch (error) {
+            console.error("Erro na consulta de dados básicos no API Contas: " + error);
+            throw new Error(msg.GC65);
+        }
+
+        return resultado;
     }
 };
